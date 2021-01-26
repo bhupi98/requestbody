c8ea9df84c5f4c9c2740631b8f87e212047c75bc
bhupender <bhupender.cool98@gmail.com>
2021-01-26 20:54:35 +0530

    added more endpoints

--- routers/routes.js
+++ routers/routes.js
@@ -1,5 +1,4 @@
 const router=require('express').Router();
-const { query } = require('express');
 const {run}=require('../config/db');
 const {schema}=require('../validation/validater')
 router.post('/api',async (req,res,next)=>{
@@ -13,11 +12,51 @@ try{
        const query=`insert into triggers values(?,?,?,?,?)`
        const arrayParams=[store.value.customerId,store.value.requesterId,store.value.notEventName,store.value.requestItemId,store.value.srcModuleName]
         const runQuery = await run(query,arrayParams)
-        res.json({message:"data inserted",runQuery})
+        res.send(200).json({message:"data inserted",runQuery})
     }
 }catch(err){
-   res.sendStatus(500)
+   console.error(err)
    next(err)
 }
+})
+router.patch("/api",async (req,res,next) => {
+    try{        
+      const store=result;                
+      const query =`update triggers set requesterId=?, notEventName=?, requestItemId=?, srcModuleName=? where customerId=?`;
+      const arrayParams =[store.value.requesterId,store.value.notEventName,store.value.requestItemId,store.value.srcModuleName,store.value.customerId]
+      const runQuery = await run(query,arrayParams)
+      res.send(200).json({message:"data updated",runQuery})
+                
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+})
+router.get("/api/:id", async(req,res,next) => {
+    try{
+        const runQuery =await run(`select * from triggers where customerId=${req.params.id}`)
+        res.status(200).json(runQuery)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+})
+router.get("/api", async(req,res,next) => {
+    try{
+        const runQuery =await run(`select * from triggers`)
+        res.status(200).json(runQuery)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+})
+router.delete("/api/:id", async(req,res,next) => {
+    try{
+        const runQuery =await run(`delete  from triggers where customerId=${req.params.id}`)
+        res.status(200).json({message:"deleted successfully",runQuery})
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
 })
 module.exports=router;
