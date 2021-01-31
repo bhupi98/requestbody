6ca92144ee827d3552f3b007cf4121c5a63a1cac
bhupender <bhupender.cool98@gmail.com>
2021-01-31 22:16:21 +0530

    changes have been made

--- validation/validater.js
+++ validation/validater.js
@@ -5,5 +5,8 @@ const schema=Joi.object({
  notEventName:Joi.string().required(),
  requestItemId:Joi.number().required(),
  srcModuleName:Joi.string().required(),
+ pccData:Joi.array().items({
+     itemData:Joi.object()
+ })
 })
 module.exports={schema:schema}
