ec8ea203e098a6090e49a4abdcd03076aae763aa
bhupender <bhupender.cool98@gmail.com>
2021-01-31 22:16:01 +0530

    changes have been made

--- routers/routes.js
+++ routers/routes.js
@@ -8,25 +8,32 @@ try{
     if(result.errors){
         return res.sendStatus(422)
     }else{
-       const store=result;
-       const query=`insert into triggers values(?,?,?,?,?)`
-       const arrayParams=[store.value.customerId,store.value.requesterId,store.value.notEventName,store.value.requestItemId,store.value.srcModuleName]
+       const data=result.value.pccData[0].itemData;
+       console.log(data)
+       const query=`insert into uno_notify_triggers (chCompanyId,vcModule,vcAction,vcTriggerJson,chStatus,nmCreatedOn,nmCreatedBy,nmUpdatedOn,nmUpdatedBy) values(?,?,?,?,?,?,?,?,?)`
+       const arrayParams=[result.value.customerId,result.value.srcModuleName,result.value.notEventName,JSON.stringify(result.value.pccData),data.status,data.createdOn,data.createdBy,data.modifiedOn,data.modifiedBy]
         const runQuery = await run(query,arrayParams)
-        res.send(200).json({message:"data inserted",runQuery})
+        res.status(200).json({message:"data inserted",runQuery})
     }
 }catch(err){
    console.error(err)
    next(err)
 }
 })
-router.patch("/api",async (req,res,next) => {
-    try{        
-      const store=result;                
-      const query =`update triggers set requesterId=?, notEventName=?, requestItemId=?, srcModuleName=? where customerId=?`;
-      const arrayParams =[store.value.requesterId,store.value.notEventName,store.value.requestItemId,store.value.srcModuleName,store.value.customerId]
-      const runQuery = await run(query,arrayParams)
-      res.send(200).json({message:"data updated",runQuery})
-                
+router.patch("/api/:id",async (req,res,next) => {
+    try{  
+        const result=schema.validate(req.body);
+        if(result.errors){
+            return res.sendStatus(422);
+        }else{
+            console.log(req.params.id)
+            const data=result.value.pccData[0].itemData;     
+            console.log(data.status)           
+            const query =`update uno_notify_triggers set chStatus=? where nmId=?`;
+            const arrayParams =[data.status,req.params.id]
+            const runQuery = await run(query,arrayParams)
+            res.status(200).json({message:"data updated",runQuery})
+                          }  
     }catch(err){
         console.error(err)
         next(err)
@@ -34,7 +41,7 @@ router.patch("/api",async (req,res,next) => {
 })
 router.get("/api/:id", async(req,res,next) => {
     try{
-        const runQuery =await run(`select * from triggers where customerId=${req.params.id}`)
+        const runQuery =await run(`select * from uno_notify_triggers where nmId=${req.params.id}`)
         res.status(200).json(runQuery)
     }catch(err){
         console.error(err)
@@ -43,7 +50,7 @@ router.get("/api/:id", async(req,res,next) => {
 })
 router.get("/api", async(req,res,next) => {
     try{
-        const runQuery =await run(`select * from triggers`)
+        const runQuery =await run(`select * from uno_notify_triggers`)
         res.status(200).json(runQuery)
     }catch(err){
         console.error(err)
@@ -52,7 +59,7 @@ router.get("/api", async(req,res,next) => {
 })
 router.delete("/api/:id", async(req,res,next) => {
     try{
-        const runQuery =await run(`delete  from triggers where customerId=${req.params.id}`)
+        const runQuery =await run(`delete  from uno_notify_triggers where nmId=${req.params.id}`)
         res.status(200).json({message:"deleted successfully",runQuery})
     }catch(err){
         console.error(err)
